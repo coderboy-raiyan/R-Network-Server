06ca2195d10d032ecccdff616aa6bf90796c0e5e
Raiyan <raiyan5764@gmail.com>
2022-06-16 20:39:46 +0600

    initial setup done

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const app = require('express')();
+
+const cors = require('cors');
+const express = require('express');
+
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log('listening server...');
+});
